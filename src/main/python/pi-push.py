d48bb1819f164459715fa3a4020308c3a4b695f6
lamon <lamoncambraia@gmail.com>
2017-06-25 19:32:46 -0300

    python pi-push fix

--- src/main/python/pi-push.py
+++ src/main/python/pi-push.py
@@ -8,5 +8,5 @@ GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 while True:
   inputValue = GPIO.input(18)
   if (inputValue == False):
-    call(["java", "-Xmx768m", "-Xms768m", "-jar", "/home/lamon/jar/web3j-test-1.0.2.jar", "web3jio0107", "/home/lamon/.ethereum/testnet/keystore/UTC--2017-06-12T14-53-36.157000000Z--b9927a78e40cedfa2b2d1bdda2ba79b9cc75e6c6.json"])
+    call(["java", "-Xmx768m", "-Xms768m", "-jar", "/home/lamon/jar/web3j-test-1.0.2.jar", "walletPassword", "/home/wallet/path/wallet.json"])
   time.sleep(0.3)
